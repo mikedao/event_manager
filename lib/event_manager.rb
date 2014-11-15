204b44eb2a9a65d68e42b6beeb5e83eb4323b2cc
Michael Dao <michael.dao@gmail.com>
2014-11-15 12:25:49 -0700

    Added sanitize zipcode code

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,17 @@
+require 'csv'
+
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.
+end
+
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  zipcode = clean_zipcode(zipcode)
+
+  puts "#{name} #{zipcode}"
 end
